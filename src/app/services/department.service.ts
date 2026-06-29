c3eadfc1d80654540633b95241c182148b8762ff
minakshikanzariya <minakshi@kanzariya.com>
2026-06-29 18:10:12 +0530

    envt

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
-apiUrl = 'http://192.168.0.34:3000';
+  private apiUrl = environment.apiUrl;
+// apiUrl = 'https://real-estate-b1.onrender.com';
 //  apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
    addDepartment(data: any): Observable<any> {
     return this.http.post(
-      `${this.apiUrl}/api/addDepartment`,
+      `${this.apiUrl}/addDepartment`,
       data
     );
   }
   getDepartments(): Observable<any> {
     return this.http.get(
-      `${this.apiUrl}/api/departments`,);
+      `${this.apiUrl}/departments`,);
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://real-estate-b1.onrender.com/api'
+};
